2b084f2a590d375d8379c93d622e84151a52f7d7
Muhammad Masood Kausar <112933892+MMK155@users.noreply.github.com>
2022-12-05 21:38:51 -0800

    status issue resolved

--- src/api/verifycontributor/controllers/verifycontributor.js
+++ src/api/verifycontributor/controllers/verifycontributor.js
@@ -1,69 +1,60 @@
 module.exports = {
-  async verify(ctx,next,plugin) {
-    const { data:{email} } = ctx.request.body;
-    console.log('email',email);
-    
-    //verify contributor
+  async verify(ctx, next, plugin) {
+    const { data: { email } } = ctx.request.body;
+
+
+    //find all users
     const entity = await strapi.entityService.findMany(
       "plugin::users-permissions.user",
     );
-      
-
-    //verify contributior email
-    var filter1= entity.filter(function (value, index, arr) {
-      return value;
-    });
 
 
-
-    const testobj = [];
-    const filter_requsted_contributor =  filter1.filter(item => item.email === email);
-    //get filter contributor detail 
-    if(filter_requsted_contributor){
-     filter_requsted_contributor.map((item)=>{
-      testobj.push(item.email)
-    });
-
-    //get filter contributor detail 
-    if(filter_requsted_contributor){
-      filter_requsted_contributor.map((item)=>{
-       testobj.push(item.id)
-       
-     });
-    }
-
-     if(filter_requsted_contributor){
-      filter_requsted_contributor.map((item)=>{
-       testobj.push(item.username)
-     });
-    }
+    const contributorArray = [];
+    const filter_requsted_contributor = entity.filter(item => item.email === email);
 
 
-    // testobj.push(item.id),
-    //   testobj.push(item.username),
-    console.log("testobj",testobj);
-    console.log("Contributor Added Successfully");
-    
-    if(testobj.length > 0){
-    const createContributor = await strapi.db.query('api::contributor.contributor').create({
-      data: {
-        email:testobj[0],
-        cid:testobj[1],
-        username:testobj[2]
-      },
-    })
-  }else{
-    console.log("Contributor Doest Exist");
-  }
+    //push requestes user data in contributorArray
+    //push email
+    if (filter_requsted_contributor) {
+      filter_requsted_contributor.map((item) => {
+        contributorArray.push(item.email)
+      });
+
+      //push id
+      if (filter_requsted_contributor) {
+        filter_requsted_contributor.map((item) => {
+          contributorArray.push(item.id)
+        });
+      }
+
+      //push username
+      if (filter_requsted_contributor) {
+        filter_requsted_contributor.map((item) => {
+          contributorArray.push(item.username)
+        });
+      }
 
 
-    }else{
+
+
+
+      //create requested contributor
+      if (contributorArray.length > 0) {
+        const createContributor = await strapi.db.query('api::contributor.contributor').create({
+          data: {
+            email: contributorArray[0],
+            cid: contributorArray[1],
+            username: contributorArray[2]
+          },
+        });
+        return createContributor;
+      } else {
+        console.log("Contributor Doest Exist");
+      }
+
+    } else {
       console.log("Error");
     }
-
-
-
-    
 
   }
 };

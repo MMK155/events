283e69d00d43dbe25bc41c5152a027a243d0e220
Muhammad Masood Kausar <112933892+MMK155@users.noreply.github.com>
2022-12-08 04:44:54 -0800

    atricle attributes format added

--- src/api/articlereports/controllers/articlereports.js
+++ src/api/articlereports/controllers/articlereports.js
@@ -1,133 +1,26 @@
 module.exports = {
   async index(ctx, next) {
     const { id, } = ctx.params;
-    
-    
     const entries = await strapi.db.query('api::article.article').findMany({
-      fields: ['id', 'title', 'url','type'],
-      select: ['id', 'title', 'url','type'],
-
-      populate: { category: true },
       where: { type: 'public' },
     });
-    ctx.body = entries;
-
-    //get objects participants from a entries array  
-    // const getobjects = entries[0].participants;
-    // const ExtractObjectFromArray = Object.values(getobjects);
-
-
-    //get participants object from ExtractObjectFromArray
-    // if(pid){
-    //   var filter1 = ExtractObjectFromArray.filter(function (value, index, arr) {
-    //     return value.attributes;
-    //   });
-    //   }
-
-     //get status true participants 
-      // var ExtractStatusTrueObject = ExtractObjectFromArray.filter(function (value, index, arr) {
-      //   return value.attributes.status == true;
-      // });
-  
-      //return status true participants 
-      // if(ExtractStatusTrueObject){
-      //     return ExtractStatusTrueObject;
-      // }
+  //array for change return  data format
+  const attributeArray = [];
+    entries.map((item) => {
+      attributeArray.push({
+        "id": item.id,
+        "attributes": {
+          "title": item.title,
+          "type": item.type,
+          "createdAt": item.createdAt,
+          "updatedAt": item.updatedAt,
+          "publishedAt": item.publishedAt,
+          "media_type": item.media_type,
+          "url": item.url,
+          "text": item.text,
+        }
+      });
+    });
+    return attributeArray;
   }
-
-
-  //udpate participants status 
-  // async updateparticipant(ctx, next){
-  //   const { id,cid } = ctx.params;
-
-  //   //get participant for update
-  //   const entries = await strapi.db.query('api::event.event').findMany({
-  //     fields: ['id', 'notes', 'participants'],
-  //     select: ['id', 'notes', 'participants'],
-  //     where: { id: id },
-  //   });
-  //   ctx.body = entries;
-   
-
-  //   //Array for participants
-  //   const unconfirm_array = [];
-    
-  //   //Get participant 
-  //   const unconfirm = entries[0].participants;
-
-  //   //conver array elements to object 
-  //   const convertToObject = Object.values(unconfirm);
-  
-  //   // filter requested participant 
-  //   if(cid){
-  //   var filter1 = convertToObject.filter(function (value, index, arr) {
-  //     return value.id == cid;
-  //   });
-  //   }
-
-  //   //new obj for requested participant 
-  //   let filter1_obj_for_udpate = {
-  //     "id": '',
-  //     "attributes": {
-  //       "name": "",
-  //       "tags": [
-  //         {
-  //           "id": 1,
-  //           "title": "Tag1"
-  //         }
-  //       ],
-  //       "email": "",
-  //       "status": false,
-  //       "attendance": false,
-  //       "contact": "",
-  //       "createdAt": "",
-  //       "updatedAt": "2022-11-20T12:29:53.048Z",
-  //       "publishedAt": "2022-11-18T12:36:01.595Z"
-  //     }
-
-  //   }
- 
-
-  //   // Udpate status of  filter1_obj_for_udpate
-  //   if (filter1) {
-  //       filter1_obj_for_udpate["id"] = filter1[0].id,
-  //       filter1_obj_for_udpate["attributes"]["name"] = filter1[0].attributes.name,
-  //       filter1_obj_for_udpate["attributes"]["tags"] = filter1[0].attributes.tags,
-  //       filter1_obj_for_udpate["attributes"]["email"] = filter1[0].attributes.email,
-  //       filter1_obj_for_udpate["attributes"]["status"] = filter1[0].attributes.status,
-  //       filter1_obj_for_udpate["attributes"]["attendance"] = true,
-  //       filter1_obj_for_udpate["attributes"]["contact"] = filter1[0].attributes.contact,
-  //       filter1_obj_for_udpate["attributes"]["createdAt"] = filter1[0].attributes.createdAt,
-  //       filter1_obj_for_udpate["attributes"]["updatedAt"] = filter1[0].attributes.updatedAt,
-  //       filter1_obj_for_udpate["attributes"]["publishedAt"] = filter1[0].attributes.publishedAt
-  //   }
-
-
-  //   //get rest of the participants from participants array
-  //   if(cid){
-  //   var filter2 = convertToObject.filter(function (value, index, arr) {
-  //     return value.id != cid;
-  //   });
-   
-
-  //   //Array for combine all participants
-  //   let ConfirmParticapantsArray = [];
-  //   ConfirmParticapantsArray.push(filter1_obj_for_udpate);
-
-  //   //get rest of participants from array
-  //   for (const i in filter2) {    
-  //       ConfirmParticapantsArray.push(filter2[i]); 
-  //   }
-
-  //   //update participants status
-  //   const entry = await strapi.db.query('api::event.event').update({
-  //     where: { id: id },
-  //     data: {
-  //       participants:ConfirmParticapantsArray
-  //     },
-  //   });
-  //   ctx.body = entry;
-
-  // } 
-  // }
-};
+}
